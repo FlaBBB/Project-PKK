38a12f59528e86335edb49bea3dba39aedde87f0
FlaBBB <91487840+FlaBBB@users.noreply.github.com>
2021-09-27 23:23:24 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Log In</title>
+        <link href="assets/login.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="die-inputs">
+                <img src="assets/logo.jfif" style="width: 30vh;">
+                <h1>LOGIN</h1>
+                <div class="der-input">
+                    <input placeholder="username" id="username" type="username">
+                </div>
+                <div class="der-input">
+                    <input placeholder="password" id="password" type="password">
+                </div>
+                <div class="der-input">
+                    <button class="btn-login">Log In</button>
+                </div>
+                <div class="forget">
+                    Belum Punya Akun? <a href=""> Daftar </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
